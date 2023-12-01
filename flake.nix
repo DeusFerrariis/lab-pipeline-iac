fd56b1dd195286a158ceae5509a10085e2e388d1
Will C IV <cleghornw04@proton.me>
2023-12-01 10:21:06 -0700

    flake init

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Development Shell for Python AWS CDK Repos";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      ( system:
+        let 
+          pkgs = import nixpkgs { inherit system; };
+        in
+        with pkgs;
+        {
+          devShells.default = mkShell {
+            buildInputs = [
+              python311
+              python311Packages.jedi
+              nodejs
+              awscli2
+            ];
+
+            shellHook = ''
+              alias cdk="npx aws-cdk"
+            '';
+          };
+        }
+      );
+}
